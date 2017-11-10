d09483fd5b9aeaf3bd731367bb0047391f0c3300
Javier Monroy Salcedo <armoniadelaberintos@gmail.com>
2017-11-09 22:03:38 -0600

    Clase validacion añadida

--- src/satseminarios/Logic/Validacion.java
+++ src/satseminarios/Logic/Validacion.java
@@ -11,4 +11,27 @@ package satseminarios.Logic;
  */
 public class Validacion {
 
+    public static boolean validarCadenas(String cadena) {
+        if (cadena.matches("[A-Za-z]{20}")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean validarTelefono(String cadena) {
+        if (cadena.matches("[0-9]{10}")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean validarCorreo(String cadena) {
+        if (cadena.matches("[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z]{2,6}")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
