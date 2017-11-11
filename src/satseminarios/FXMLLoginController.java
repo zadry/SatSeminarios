996cc873d3347e6363317dbc793728ff4f50077b
Javier Monroy Salcedo <armoniadelaberintos@gmail.com>
2017-11-10 23:01:10 -0600

    pantalla registrar Candidatos subir fotos y pdf amedias

--- src/satseminarios/FXMLLoginController.java
+++ src/satseminarios/FXMLLoginController.java
@@ -5,9 +5,22 @@
  */
 package satseminarios;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -16,12 +29,63 @@ import javafx.fxml.Initializable;
  */
 public class FXMLLoginController implements Initializable {
 
+    @FXML
+    private AnchorPane mainPane;
+    @FXML
+    private Label lbUser;
+    @FXML
+    private TextField tfUsuario;
+    @FXML
+    private Label lbPass;
+    @FXML
+    private TextField pass;
+    @FXML
+    private Label lbInfo;
+    @FXML
+    private Button submit;
+    @FXML
+    private Button registrarse;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+    }
+
+    @FXML
+    private void handlerSubmit(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlerRegistrarse(ActionEvent event) throws IOException {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("AVISO");
+        alert.setHeaderText("Escoja la acción que desea realizar");
+        alert.setContentText("Escoja una opción");
+
+        ButtonType buttonTypeOne = new ButtonType("Registrar Candidato");
+        ButtonType buttonTypeTwo = new ButtonType("Registrar Asesor");
+
+        ButtonType buttonTypeCancel = new ButtonType("Cancel", ButtonData.CANCEL_CLOSE);
+
+        alert.getButtonTypes().setAll(buttonTypeOne, buttonTypeTwo, buttonTypeCancel);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == buttonTypeOne) {
+            // ... user chose "One"
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("RegistrarCandidatos.fxml"));
+            mainPane.getChildren().setAll(pane);
+
+        } else if (result.get() == buttonTypeTwo) {
+            // ... user chose "Two"
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("RegistrarAsesor.fxml"));
+            mainPane.getChildren().setAll(pane);
+        } else {
+            // ... user chose CANCEL or closed the dialog
+        }
+
     }
 
 }
--- src/satseminarios/RegistrarCandidatosController.java
+++ src/satseminarios/RegistrarCandidatosController.java
@@ -5,14 +5,27 @@
  */
 package satseminarios;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javax.imageio.ImageIO;
 
 /**
  * FXML Controller class
@@ -45,6 +58,8 @@ public class RegistrarCandidatosController implements Initializable {
     private Button btAnexarCartaCompromiso;
     @FXML
     private Button btAnexarCartaMotivos;
+    @FXML
+    private ImageView ivFoto;
 
     /**
      * Initializes the controller class.
@@ -55,11 +70,78 @@ public class RegistrarCandidatosController implements Initializable {
     }
 
     @FXML
-    private void handlerAnexarFoto(ActionEvent event) {
+    private void handlerAnexarFoto(ActionEvent event) throws IOException {
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().addAll(new ExtensionFilter("Image Files", "*.jpg", "*.png", "*.bitmap", "*.gif"));
+        File selectedFile = fileChooser.showOpenDialog(null);
+
+        if (selectedFile != null) {
+
+            Image image = new Image(selectedFile.toURI().toString());
+            ivFoto.setImage(image);
+            BufferedImage bImage = SwingFXUtils.fromFXImage(image, null);
+            ByteArrayOutputStream s = new ByteArrayOutputStream();
+            ImageIO.write(bImage, "png", s);
+            //ress es la variable a guardar en la base de datos como binario
+            byte[] res = s.toByteArray();
+            s.close();
+            System.out.println(res.length);
+
+            // String photo = selectedFile.toURI().toString();
+            //System.out.println(selectedFile.toURI());
+            //actionStatus.setText("File selected: " + selectedFile.getName());
+        } else {
+            //actionStatus.setText("File selection cancelled.");
+
+        }
+
     }
 
     @FXML
-    private void handlerBtCartaCompromiso(ActionEvent event) {
+    private void handlerBtCartaCompromiso(ActionEvent event) throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().addAll(new ExtensionFilter("PDF Files", "*.pdf"));
+        File selectedFile = fileChooser.showOpenDialog(null);
+
+        if (selectedFile != null) {
+
+            InputStream inputStream = null;
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            try {
+                System.out.println(selectedFile.toURI().toString());
+                File file = new File(selectedFile.toURI().toString());
+                inputStream = new FileInputStream(file);
+
+                byte[] buffer = new byte[1024];
+                baos = new ByteArrayOutputStream();
+
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    baos.write(buffer, 0, bytesRead);
+                }
+                System.out.println(bytesRead);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            // String photo = selectedFile.toURI().toString();
+            //System.out.println(selectedFile.toURI());
+            //actionStatus.setText("File selected: " + selectedFile.getName());
+        } else {
+            //actionStatus.setText("File selection cancelled.");
+
+        }
     }
 
     @FXML
