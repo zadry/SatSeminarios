41b4f8adbc55f26aea236d0d4f001647de440dad
Javier Monroy Salcedo <armoniadelaberintos@gmail.com>
2017-11-10 20:37:22 -0600

    pantalla registrar Candidatos2

--- src/satseminarios/SATSeminarios.java
+++ src/satseminarios/SATSeminarios.java
@@ -5,6 +5,9 @@
  */
 package satseminarios;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
